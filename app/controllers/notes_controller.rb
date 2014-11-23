adca3ac2d75851c2563480597323c7f2739d7a7f
yamamoty <itsuko.y0214@gmail.com>
2014-11-23 22:57:23 +0900

    第四回自宅学習
    edit, updateの実装完了

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,32 @@
 class NotesController < ApplicationController
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render :edit
+    end 
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+     end
+  
+  def index
+    @notes= Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
   def new
     @note = Note.new
   end
   
   def create
-    @note = Note.new(group_params)
+    @note = Note.new(note_params)
     if @note.save
       redirect_to @note
     else
